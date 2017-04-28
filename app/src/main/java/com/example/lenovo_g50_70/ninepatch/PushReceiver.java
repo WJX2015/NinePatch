272ed32ac7da8711adf12fd3b4b0b38988bd3245
wjx <550481089@qq.com>
2017-04-28 14:47:23 +0800

    聊天界面+百度云推送

---
+++ app/src/main/java/com/example/lenovo_g50_70/ninepatch/PushReceiver.java
@@ -0,0 +1,54 @@
+package com.example.lenovo_g50_70.ninepatch;
+
+import android.content.Context;
+
+import com.baidu.android.pushservice.PushMessageReceiver;
+
+import java.util.List;
+
+/**
+ * 百度云推送自定义回调类
+ * Created by lenovo-G50-70 on 2017/4/27.
+ */
+
+public class PushReceiver extends PushMessageReceiver {
+    @Override
+    public void onBind(Context context, int i, String s, String s1, String s2, String s3) {
+
+    }
+
+    @Override
+    public void onUnbind(Context context, int i, String s) {
+
+    }
+
+    @Override
+    public void onSetTags(Context context, int i, List<String> list, List<String> list1, String s) {
+
+    }
+
+    @Override
+    public void onDelTags(Context context, int i, List<String> list, List<String> list1, String s) {
+
+    }
+
+    @Override
+    public void onListTags(Context context, int i, List<String> list, String s) {
+
+    }
+
+    @Override
+    public void onMessage(Context context, String s, String s1) {
+
+    }
+
+    @Override
+    public void onNotificationClicked(Context context, String s, String s1, String s2) {
+
+    }
+
+    @Override
+    public void onNotificationArrived(Context context, String s, String s1, String s2) {
+
+    }
+}
